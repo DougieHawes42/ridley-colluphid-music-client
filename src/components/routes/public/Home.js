c4da8653f07295391f1c982a5da93fb6b639827b
dougiehawes <dougiehawes@hotmail.com>
2026-02-21 12:38:29 +0000

    connecting back end

--- src/components/routes/public/Home.js
+++ src/components/routes/public/Home.js
@@ -1,7 +1,25 @@
+import axios from "axios";
+
+import { useState, useEffect } from "react";
+
 import "./style.scss";
 
-const Home = ({ darkmode }) => {
-  return <div className="Home">Home</div>;
+const Home = () => {
+  const [welcomeMessage, setWelcomeMessage] = useState("");
+
+  useEffect(() => {
+    const fetchMessage = async () => {
+      const fetchedMessage = await axios.get(
+        `${process.env.REACT_APP_API_URL}/api`,
+      );
+
+      setWelcomeMessage(fetchedMessage.data.message);
+    };
+
+    fetchMessage();
+  }, []);
+
+  return <div className="Home">Home - {welcomeMessage}</div>;
 };
 
 export default Home;
